08b6c465cd9df512e2157d652ba157898be0c681
Jevin Sweval <jevinsweval@gmail.com>
2021-06-16 23:57:00 +0000

    streamer added but doesnt do anything

--- stream-test/stream-sim.py
+++ stream-test/stream-sim.py
@@ -169,8 +169,8 @@ class BenchSoC(SoCCore):
         self.submodules.icmp = LiteEthICMP(self.ip, etherbone_ip_address, dw=8)
         self.submodules.udp = LiteEthUDP(self.ip, etherbone_ip_address, dw=8)
 
-        # udp_port = self.udp.crossbar.get_port(1234, dw=8)
-        # self.submodules.streamer = Streamer(self.platform.request("streamer"), udp_port)
+        udp_port = self.udp.crossbar.get_port(1234, dw=8)
+        self.submodules.streamer = Streamer(self.platform.request("streamer"), udp_port)
 
         if sim_debug:
             platform.add_debug(self, reset=1 if trace_reset_on else 0)
