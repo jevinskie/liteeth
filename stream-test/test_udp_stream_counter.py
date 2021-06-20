aed804dada780f12f0d8f4aad4359cc3450cc1bd
Jevin Sweval <jevinsweval@gmail.com>
2021-06-20 04:26:15 +0000

    packet rx test util

--- stream-test/test_udp_stream_counter.py
+++ stream-test/test_udp_stream_counter.py
@@ -11,9 +11,21 @@ from stream_common import *
 
 def test_udp_stream_counter():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_address = (streamer_target_ip_address, streamer_port)
-    data, server = sock.recvfrom(4096)
-    print(f'len(data): {len(data)} server: {server}')
+    sock.bind(('0.0.0.0', streamer_port))
+    last_int = None
+    payload_sz = 8
 
-if __name__ == '__nain__':
+    while True:
+        data, server = sock.recvfrom(4096)
+        da = array.array('Q', data)
+        print(f'len(data): {len(data)} da[0]: {da[0]:#x}')
+
+        for i in range(1, len(da)):
+            assert da[i-1] + 1 == da[i]
+
+        if last_int is not None:
+            assert last_int + 1 == da[0]
+        last_int = da[-1]
+
+if __name__ == '__main__':
     sys.exit(test_udp_stream_counter())
