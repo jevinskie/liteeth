d35e7f4a5f7e348a55f410f43d48f57f213e295f
Jevin Sweval <jevinsweval@gmail.com>
2021-09-23 00:36:57 -0700

    wip

--- io-delay-tests/altera_max10_dev_kit.py
+++ io-delay-tests/altera_max10_dev_kit.py
@@ -31,7 +31,7 @@ from litescope import LiteScopeAnalyzer
 # CRG ----------------------------------------------------------------------------------------------
 
 class _CRG(Module):
-    def __init__(self, platform, sys_clk_freq, with_rgmii_pll=False, rgmii_tx_delay=2e-9, with_adc_pll=False):
+    def __init__(self, platform, sys_clk_freq, with_rgmii_pll=False, rgmii_tx_delay=2e-9):
         self.rst = Signal()
         self.clock_domains.cd_sys    = ClockDomain()
 
@@ -57,14 +57,6 @@ class _CRG(Module):
 
         pll.create_clkout(self.cd_sys, sys_clk_freq)
 
-        if with_adc_pll:
-            self.clock_domains.cd_adc = ClockDomain()
-            clk10_adc = platform.request("clk10_adc")
-            self.submodules.pll_adc = pll_adc = Max10PLL(speedgrade="-6")
-            self.comb += pll_adc.reset.eq(self.rst)
-            pll_adc.register_clkin(clk10_adc, 10e6)
-            pll_adc.create_clkout(self.cd_adc, 10e6)  # first so it uses clkout[0]
-
 # BaseSoC ------------------------------------------------------------------------------------------
 
 class BareSoC(SoCCore):
@@ -74,7 +66,7 @@ class BareSoC(SoCCore):
 
         # SoCMini ----------------------------------------------------------------------------------
         SoCMini.__init__(self, platform, sys_clk_freq,
-            ident         = "LiteX SoC on Altera's Max 10 dev kit",
+            ident         = "LiteX BareSoC on Altera's Max 10 dev kit RGMII IODELAY EDITION",
             ident_version = True,
             **kwargs)
 
@@ -94,35 +86,30 @@ class BareSoC(SoCCore):
 class BaseSoC(SoCCore):
     def __init__(self,
                  sys_clk_freq=int(125e6),
-                 with_led_chaser     = True,
-                 with_jtagbone       = False,
-                 with_uartbone       = False,
-                 with_ethernet       = False,
-                 with_etherbone      = False,
-                 with_gigabone       = False,
-                 eth_ip              = "192.168.43.50",
-                 eth_dynamic_ip      = False,
-                 with_analyzer       = False,
-                 with_adc            = False,
                  **kwargs):
         self.platform = platform = altera_max10_dev_kit.Platform()
 
         # SoCCore ----------------------------------------------------------------------------------
-        SoCCore.__init__(self, platform, sys_clk_freq,
-            ident         = "LiteX SoC on Altera's Max 10 dev kit",
+        SoCMini.__init__(self, platform, sys_clk_freq,
+            ident         = "LiteX SoC on Altera's Max 10 dev kit RGMII IODELAY EDITION",
             ident_version = True,
             **kwargs)
 
         # CRG --------------------------------------------------------------------------------------
-        self.submodules.crg = _CRG(platform, sys_clk_freq, with_adc_pll=with_adc, with_rgmii_pll=True)
+        self.submodules.crg = _CRG(platform, sys_clk_freq, with_rgmii_pll=True)
 
         # Jtagbone ---------------------------------------------------------------------------------
-        if with_jtagbone:
-            self.add_jtagbone()
+        self.add_jtagbone()
 
-        # UARTbone
-        if with_uartbone:
-            self.add_uartbone(baudrate=3_000_000)
+        two_led_pads = Cat(platform.request("user_led"), platform.request("user_led"))
+        self.submodules.leds = LedChaser(
+            pads=two_led_pads,
+            sys_clk_freq=sys_clk_freq)
+
+        sink_led_pads = Record([
+            ("led2", platform.request("user_led")),
+            ("led3", platform.request("user_led")),
+        ], "two_sink_leds")
 
         # Ethernet MDIO
         self.submodules.mdio = LiteEthPHYMDIO(self.platform.request("eth_mdio"))
@@ -131,55 +118,29 @@ class BaseSoC(SoCCore):
         eth_other = self.platform.request("eth_other")  # make sure all PHY pads are included to get the IO levels right
         eth_clock_pads0 = self.platform.request("eth_clocks")
         eth_pads0 = self.platform.request("eth")
-        if with_ethernet or with_etherbone:
-            self.add_constant("USE_ALT_MODE_FOR_88E1111")
-            self.add_constant("ALT_MODE_FOR_88E1111_PHYADDR0", 0)
-            self.add_constant("ALT_MODE_FOR_88E1111", 0b1111)  # HW_CONFIG GMII
-
-            self.submodules.ethphy = LiteEthPHYMII(
-                clock_pads = eth_clock_pads0,
-                pads       = eth_pads0)
-            self.ethphy = ClockDomainsRenamer({"eth_rx": "ethphy_rx", "eth_tx": "ethphy_tx"})(self.ethphy)
-
-            if with_ethernet:
-                self.add_ethernet(phy=self.ethphy, phy_cd="ethphy", dynamic_ip=eth_dynamic_ip)
-            if with_etherbone:
-                self.add_etherbone(phy=self.ethphy, phy_cd="ethphy", ip_address=eth_ip)
 
         eth_clock_pads1 = self.platform.request("eth_clocks")
         eth_pads1 = self.platform.request("eth")
-        if with_gigabone:
-            self.add_constant("USE_DELAY_MODE_FOR_88E1111")
-            self.add_constant("DELAY_MODE_FOR_88E1111_PHYADDR0", 1)
+        self.submodules.ethphy1 = LiteEthPHYRGMII(
+            clock_pads = eth_clock_pads1,
+            pads       = eth_pads1,
+            cd_eth_rx  = self.crg.cd_ethphy1_rx,
+            cd_eth_tx  = self.crg.cd_ethphy1_tx)
+        self.ethphy1 = ClockDomainsRenamer({"eth_rx": "ethphy1_rx",
+                                            "eth_tx": "ethphy1_tx",
+                                            "eth_tx_delayed": "ethphy1_tx_delayed"})(self.ethphy1)
 
-            self.submodules.ethphy1 = LiteEthPHYRGMII(
-                clock_pads = eth_clock_pads1,
-                pads       = eth_pads1,
-                cd_eth_rx  = self.crg.cd_ethphy1_rx,
-                cd_eth_tx  = self.crg.cd_ethphy1_tx)
-            self.ethphy1 = ClockDomainsRenamer({"eth_rx": "ethphy1_rx",
-                                                "eth_tx": "ethphy1_tx",
-                                                "eth_tx_delayed": "ethphy1_tx_delayed"})(self.ethphy1)
 
-            import socket
-            a0, a1, a2, a3 = socket.inet_aton(eth_ip)
-            eth_ip1 = socket.inet_ntoa(bytes([a0, a1, a2+1, a3+1]))
-            if True:
-                self.add_etherbone(name="gigabone1",
-                                   phy=self.ethphy1,
-                                   phy_cd="ethphy1",
-                                   mac_address=0x10e2d5000000+1,
-                                   ip_address=eth_ip1,
-                                   dummy_checksum=True)
-                self.platform.toolchain.additional_qsf_commands.append('''
+        self.platform.toolchain.additional_qsf_commands.append('''
+set_instance_assignment -name DUAL_PURPOSE_CLOCK_PIN_DELAY 11 -to eth_clocks_rx_1
 # set_instance_assignment -name PAD_TO_CORE_DELAY 6 -to eth_clocks_rx_1
 set_instance_assignment -name PAD_TO_CORE_DELAY 0 -to eth_rx_ctl
-set_instance_assignment -name PAD_TO_CORE_DELAY 0 -to eth_rx_data_1[0]
-set_instance_assignment -name PAD_TO_CORE_DELAY 0 -to eth_rx_data_1[1]
-set_instance_assignment -name PAD_TO_CORE_DELAY 0 -to eth_rx_data_1[2]
-set_instance_assignment -name PAD_TO_CORE_DELAY 0 -to eth_rx_data_1[3]
+set_instance_assignment -name PAD_TO_CORE_DELAY 6 -to eth_rx_data_1[0]
+set_instance_assignment -name PAD_TO_CORE_DELAY 3 -to eth_rx_data_1[1]
+set_instance_assignment -name PAD_TO_CORE_DELAY 2 -to eth_rx_data_1[2]
+set_instance_assignment -name PAD_TO_CORE_DELAY 4 -to eth_rx_data_1[3]
 ''')
-                self.platform.toolchain.additional_sdc_commands.append('''
+        self.platform.toolchain.additional_sdc_commands.append('''
 # set_input_delay -add_delay  -clock [get_clocks {main_ethphy1_clkbuf_cd_ethphy1_rx_clk_out}]  -13.5 [get_nodes {eth_clocks_rx_1}]
 
 # set_input_delay -add_delay  -clock [get_clocks {main_ethphy1_clkbuf_cd_ethphy1_rx_clk_out}]  -max -1.4 [get_ports {eth_rx_ctl}]
@@ -238,11 +199,11 @@ post_message -type info "Reading file: \'rgmii_input.sdc\'"
 #**************************************************************
 set Tco_max 0.5
 set Tco_min -0.5
-set Td_max 0.4
+set Td_max 0.0
 set Td_min 0.0
 set longest_src_clk 0.0
 set shortest_src_clk 0.0
-set longest_dest_clk 0.4
+set longest_dest_clk 0.0
 set shortest_dest_clk 0.0
 
 set Tcs_largest [expr $shortest_dest_clk - $longest_src_clk]
@@ -289,53 +250,33 @@ set_false_path \
 
 ''')
 
-            else:
-                # Timing constraints
-                eth_rx_clk = self.crg.cd_ethphy1_rx.clk
-                eth_tx_clk = self.crg.cd_ethphy1_tx.clk
-                self.platform.add_period_constraint(eth_rx_clk, 1e9 / self.ethphy1.rx_clk_freq)
-                self.platform.add_period_constraint(eth_tx_clk, 1e9 / self.ethphy1.tx_clk_freq)
-                self.platform.add_false_path_constraints(self.crg.cd_sys.clk, eth_rx_clk, eth_tx_clk)
-                self.platform.associate_clock_and_pad(eth_rx_clk, self.ethphy1.clock_pads.rx)
-                self.platform.associate_clock_and_pad(eth_tx_clk, self.ethphy1.clock_pads.tx)
-
-        # ADC --------------------------------------------------------------------------------------
-        if with_adc:
-            self.submodules.adc = Max10ADC(adc_num=0)
-            self.adc.do_finalize()
-            self.platform.add_false_path_constraints(self.crg.cd_sys.clk, self.crg.cd_adc.clk)
-
         # Analyzer ---------------------------------------------------------------------------------
-        if with_analyzer:
-            analyzer_signals = {
-                *self.ethphy1._signals,
-                self.ethphy1.crg.rx_cnt, self.ethphy1.crg.tx_cnt,
-                # *self.ethphy._signals_recursive,
-                # *self.ethcore.icmp.echo._signals, *self.ethcore.icmp.rx._signals, *self.ethcore.icmp.tx._signals,
-                # *self.gigabone1_ethcore.arp.rx._signals, *self.gigabone1_ethcore.arp.tx._signals,
-                # *self.ethcore.mac.core._signals,
-                # eth_clock_pads,
-                # *eth_pads1,
-                # *self.adc._signals,
-            }
-            analyzer_signals_denylist = {
-                self.ethphy1.clock_pads, eth_pads1.tx_data, eth_pads1.tx_ctl,
-                # self.gigabone1_ethcore.arp.rx.depacketizer.header,
-                # self.gigabone1_ethcore.arp.tx.packetizer.header,
-            }
-            analyzer_signals -= analyzer_signals_denylist
-            analyzer_signals = list(analyzer_signals)
-            self.submodules.analyzer = LiteScopeAnalyzer(analyzer_signals,
-                depth        = 256,
-                clock_domain = "ethphy1_rx",
-                register     = True,
-                csr_csv      = "analyzer.csv")
-
-        # Leds -------------------------------------------------------------------------------------
-        if with_led_chaser:
-            self.submodules.leds = LedChaser(
-                pads         = platform.request_all("user_led"),
-                sys_clk_freq = sys_clk_freq)
+        analyzer_signals = {
+            *self.ethphy1._signals,
+            self.ethphy1.crg.rx_cnt, self.ethphy1.crg.tx_cnt,
+        }
+        analyzer_signals_denylist = {
+            self.ethphy1.clock_pads, eth_pads1.tx_data, eth_pads1.tx_ctl,
+        }
+        analyzer_signals -= analyzer_signals_denylist
+        analyzer_signals = list(analyzer_signals)
+        led2 = Signal()
+        for sig in analyzer_signals:
+            if isinstance(sig, Record):
+                for subsig in sig.flatten():
+                    self.comb += led2.eq(led2 ^ subsig)
+            elif len(sig) > 1:
+                for i in range(len(sig)):
+                    self.comb += led2.eq(led2 ^ sig[i])
+            else:
+                self.comb += led2.eq(led2 ^ sig)
+        self.comb += sink_led_pads.led2.eq(led2)
+        self.comb += sink_led_pads.led3.eq(~led2)
+        # self.submodules.analyzer = LiteScopeAnalyzer(analyzer_signals,
+        #     depth        = 256,
+        #     clock_domain = "ethphy1_rx",
+        #     register     = True,
+        #     csr_csv      = "analyzer.csv")
 
 
 # Build --------------------------------------------------------------------------------------------
@@ -353,16 +294,6 @@ def main():
     parser.add_argument("--build",               action="store_true", help="Build bitstream")
     parser.add_argument("--load",                action="store_true", help="Load bitstream")
     parser.add_argument("--sys-clk-freq",        default=125e6,        help="System clock frequency (default: 50MHz)")
-    parser.add_argument("--with-jtagbone",       action="store_true", help="Enable JTAGbone support")
-    parser.add_argument("--with-uartbone",       action="store_true", help="Enable UARTbone support")
-    ethopts = parser.add_mutually_exclusive_group()
-    ethopts.add_argument("--with-ethernet",      action="store_true", help="Enable 100 Mbps Ethernet support on port A")
-    ethopts.add_argument("--with-etherbone",     action="store_true", help="Enable 100 Mbps Etherbone support on port A")
-    parser.add_argument("--with-gigabone",       action="store_true", help="Enable Gigabit Etherbone support on port B")
-    parser.add_argument("--eth-ip",              default="192.168.43.50", type=str, help="Ethernet/Etherbone IP address")
-    parser.add_argument("--eth-dynamic-ip",      action="store_true", help="Enable dynamic Ethernet IP addresses setting")
-    parser.add_argument("--with-adc",            action="store_true", help="Enable ADC and internal temperature support")
-    parser.add_argument("--with-analyzer",       action="store_true", help="Enable Analyzer support")
     builder_args(parser)
     soc_core_args(parser)
     argparse_set_def(parser, 'csr_csv', 'csr.csv')
@@ -375,19 +306,8 @@ def main():
 
     args = parser.parse_args()
 
-    assert not (args.with_etherbone and args.eth_dynamic_ip)
-
     soc = BaseSoC(
         sys_clk_freq             = int(float(args.sys_clk_freq)),
-        with_jtagbone            = args.with_jtagbone,
-        with_uartbone            = args.with_uartbone,
-        with_ethernet            = args.with_ethernet,
-        with_etherbone           = args.with_etherbone,
-        with_gigabone            = args.with_gigabone,
-        eth_ip                   = args.eth_ip,
-        eth_dynamic_ip           = args.eth_dynamic_ip,
-        with_analyzer            = args.with_analyzer,
-        with_adc                 = args.with_adc,
         **soc_core_argdict(args)
     )
     builder = Builder(soc, **builder_argdict(args))
